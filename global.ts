95841237ed0c9d255c366d572040b00418163d5a
Ghyath <ghyath.darwish@gmail.com>
2021-10-22 15:51:43 +0300

    remove local session package

--- global.ts
+++ global.ts
@@ -1,9 +1,7 @@
 import { Telegraf } from "telegraf";
-import LocalSession from "telegraf-session-local";
 import { MyContext } from "./@types";
 import IG from "./instagram";
 const bot = new Telegraf<MyContext>(process.env.BOT_TOKEN!)
-bot.use((new LocalSession({storage: LocalSession.storageMemory, property:'session' })).middleware())
 export const randomItem = (items:any[])=>{
     return items[Math.floor(Math.random()*items.length)];
 }
