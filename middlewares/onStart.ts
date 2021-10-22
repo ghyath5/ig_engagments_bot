1448978da992904868b4a8053a352e4343a0b9e4
Ghyath <ghyath.darwish@gmail.com>
2021-10-22 23:10:38 +0300

    stop bot

--- middlewares/onStart.ts
+++ middlewares/onStart.ts
@@ -5,6 +5,9 @@ import { PrismaClient } from '@prisma/client';
 import i18n from "../locales";
 const prisma = new PrismaClient()
 bot.use(async (ctx,next)=>{
+    if(!Boolean(process.env.STATUS)){
+        return ctx.replyWithHTML("تمت جميع عملياتك اليوم, حاول غداً");
+    }
     if(!ctx.from?.id || ctx.from.is_bot)return;
     ctx.pk = ctx.from.id;
     ctx.i18n = i18n;
