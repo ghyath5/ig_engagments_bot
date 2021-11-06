7cd88bde32ed7a11683812c8114493ac33291e66
Ghyath <ghyath.darwish@gmail.com>
2021-11-06 14:55:39 +0200

    delete unfollow detected

--- utls.ts
+++ utls.ts
@@ -1,6 +1,6 @@
-import { Account, User } from ".prisma/client";
+import { Account, PrismaClient, User } from ".prisma/client";
 import { Client } from "./client";
-
+const prisma = new PrismaClient()
 export const notifyUnfollowers = async (pk:number,users:(Account & {follower: User})[])=>{
     let user = new Client(pk);
     let username = await user.getUsername()
@@ -8,15 +8,30 @@ export const notifyUnfollowers = async (pk:number,users:(Account & {follower: Us
     function sendNotification(){
         let timeoutcounter = setTimeout(async ()=>{
             if(i >= users.length){
-                user.addGems(users.length);
+                user.addGems(users.length-2);
                 clearTimeout(timeoutcounter);
-                return user.translate('yougotgems',{gems:users.length-2})
+                return user.translate('yougotgems',{gems:users.length-2}).send()
             }
-            let client = new Client(users[i].follower.id);
-            await client.getLang();
-            await client.translate('detectUnfollow',{name:username}).send()
+            let active = users[i];
+            let client = new Client(active.follower.id);
+            // await client.getLang();
+            // await client.translate('detectUnfollow',{name:username}).send()
             console.log(`Sending ${i+1}/${users.length}`);
-            client.deductGems(1);
+            prisma.account.delete({
+                where:{
+                    followed_id_follower_id:{
+                        followed_id:active.followed_id,
+                        follower_id:active.follower_id,
+                    }
+                }
+            }).catch((e)=>{
+                console.log(e);
+                
+            }).then((a)=>{
+                console.log('Deleted ', i);
+                
+            })
+            // client.deductGems(1);
             i++;
             sendNotification();
         },500)
