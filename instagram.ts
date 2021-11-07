242c1fdd3a95b835e40816939558451cb9e504e8
Ghyath <ghyath.darwish@gmail.com>
2021-11-07 14:30:12 +0200

    reduce waiting time after falur

--- instagram.ts
+++ instagram.ts
@@ -222,7 +222,7 @@ class IG {
                 bot.telegram.sendMessage(adminId,`Error at Proxy: ${this.proxy.ip}\nProxies Number: ${proxyIndex+1}/${(await proxies.get()).length} Error: ${( e as any).message}`)
                 if(!e.response || ( e as any).message?.includes("429")){
                     await proxies.remove(this.proxy);
-                    await this.sleep(8000);
+                    await this.sleep(2000);
                     return resolve(await this.getFollowing(id,cursor));
                 }
                 return resolve(null);
