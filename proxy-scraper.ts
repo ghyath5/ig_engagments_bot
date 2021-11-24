e1eecca23406a79dd700038e9ce6f240d73a8e03
Ghyath <ghyath.darwish@gmail.com>
2021-11-24 16:49:54 +0200

    enhance scraper

--- proxy-scraper.ts
+++ proxy-scraper.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { adminId, bot } from "./global";
 import * as Tunnel from 'tunnel';
-import { proxies } from "./instagram";
+import IG, { proxies } from "./instagram";
 class ProxyScraper{
     limit:number = 400;
+    checkTimes:number = 2;
     current:number = 0;
     all:number;
     working:{ip:string,port:string}[]=[]
-    constructor({limit}:{limit:number}){
+    constructor({limit,checkTimes}:{limit:number,checkTimes:number}){
         this.limit = limit;
+        this.checkTimes = checkTimes
     }
     async start(){
         try{
@@ -19,13 +21,13 @@ class ProxyScraper{
                 if(!p)return;
                 let splitedText = p.split(':')
                 let proxy:{ip:string,port:string} = {ip:splitedText[0],port:splitedText[1]}
-                this.check(proxy)
+                this.checker(proxy)
             })
         }catch(e){
             bot.telegram.sendMessage(adminId,`Scraping Error:${(e as any).message}`)
         }
     }
-    check(proxy:{ip:string,port:string},url='https://www.instagram.com'){
+    async checker(proxy:{ip:string,port:string},url='https://www.instagram.com'){
         console.log('Checking...',proxy.ip,proxy.port);
         let tunnel = Tunnel.httpsOverHttp({
             proxy: {
@@ -33,31 +35,49 @@ class ProxyScraper{
                 port: Number(proxy.port)
             },
         });
-        const source = axios.CancelToken.source();
-        const timeout = setTimeout(() => {
-          source.cancel('timeout');
-        }, 8000);
-        axios(url,
-            {
-            proxy:false,
-            cancelToken: source.token,
-            timeout:8000,
-            ...(tunnel&&{httpsAgent:tunnel,httpAgent:tunnel})
-            }
-        ).then(()=>{
-           this.working.push(proxy)
-        })
-        .catch((e)=>{}).finally(()=>{
-            this.current++;
-            clearTimeout(timeout)
-            if(this.current >= this.all){
-                bot.telegram.sendMessage(adminId,`Saving ${this.working.length} proxies`)
-                proxies.push(this.working)
-            }
-        })
+        let checkTime = 1;
+        const check = async ()=>{
+            console.log('Check time',checkTime);
+            
+            const source = axios.CancelToken.source();
+            const timeout = setTimeout(() => {
+                source.cancel('timeout');
+            }, 8000);
+            return new Promise((resolve)=>{
+                axios(url,
+                    {
+                    proxy:false,
+                    cancelToken: source.token,
+                    timeout:8000,
+                    ...(tunnel&&{httpsAgent:tunnel,httpAgent:tunnel})
+                    }
+                ).then(async ()=>{
+                    if(this.checkTimes>checkTime){
+                        checkTime++;
+                        await IG.sleep(500,2000)
+                        return resolve(check())
+                    }
+                    return resolve(true);
+                })
+                .catch((e)=>{
+                    return resolve(false)
+                }).finally(()=>{
+                    clearTimeout(timeout)
+                })
+            })
+        }
+        let state = await check()
+        if(state){
+            this.working.push(proxy);
+        }
+        this.current++;
+        if(this.all <= this.current && this.working.length>0){
+            proxies.push(this.working)
+            bot.telegram.sendMessage(adminId,`Saving ${this.working.length} proxies`)
+        }
     }
 }
 setInterval(()=>{
-    let scraper = new ProxyScraper({limit:500})
+    let scraper = new ProxyScraper({limit:500,checkTimes:2})
     scraper.start()
-},60000*10)
+},60000*5)
