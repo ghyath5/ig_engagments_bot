3be65562b46abeb5df9dfaf475da02cc053bbbce
Ghyath Ubuntu <ghyath.darwish@gmail.com>
2021-12-02 19:06:01 +0200

    Pretify

--- utls.ts
+++ utls.ts
@@ -1,61 +1,61 @@
-import {  Account, Follow, PrismaClient } from ".prisma/client";
+import { Account, Follow, PrismaClient } from ".prisma/client";
 import { Client } from "./client";
 const prisma = new PrismaClient()
-export const notifyUnfollowers = async (pk:number,users:(Follow & {follower: Account | null})[])=>{
+export const notifyUnfollowers = async (pk: number, users: (Follow & { follower: Account | null })[]) => {
     let user = new Client(pk);
-    let [username,lang] = await Promise.all([
+    let [username, lang] = await Promise.all([
         user.getUsername(),
         user.getLang()
     ])
     let i = 0;
-    await user.addGems((users.length)-4);
-    user.translate('yougotgems',{gems:(users.length)}).send()
-    function sendNotification(){
-        let timeoutcounter = setTimeout(async ()=>{
-            if(i >= users.length){
+    await user.addGems((users.length));
+    user.translate('yougotgems', { gems: (users.length) }).send()
+    function sendNotification() {
+        let timeoutcounter = setTimeout(async () => {
+            if (i >= users.length) {
                 clearTimeout(timeoutcounter);
                 return;
             }
             let active = users[i];
             let client = new Client(active.follower!.user_id);
-            console.log(`Sending ${i+1}/${users.length}`);
+            console.log(`Sending ${i + 1}/${users.length}`);
             prisma.follow.delete({
-                where:{
-                    followed_id_follower_id:{
-                        followed_id:active!.followed_id,
-                        follower_id:active!.follower_id,
+                where: {
+                    followed_id_follower_id: {
+                        followed_id: active!.followed_id,
+                        follower_id: active!.follower_id,
                     }
                 },
-                include:{
-                    follower:true
+                include: {
+                    follower: true
                 }
-            }).then(async(a)=>{
+            }).then(async (a) => {
                 client.deductGems(2);
                 await client.getLang();
-                await client.translate('detectUnfollow',{name:username}).send()
+                await client.translate('detectUnfollow', { name: username }).send()
             })
             i++;
             sendNotification();
-        },1000)
+        }, 1000)
     }
     sendNotification()
 }
 
-export const multipleNotification = async (ids:number[]|string[],callback:(client:Client)=>void)=>{
+export const multipleNotification = async (ids: number[] | string[], callback: (client: Client) => void) => {
     let i = 0;
-    function sendNotification(){
-        let timeoutcounter = setTimeout(async ()=>{
-            if(i >= ids.length){
+    function sendNotification() {
+        let timeoutcounter = setTimeout(async () => {
+            if (i >= ids.length) {
                 return clearTimeout(timeoutcounter);
             }
             let active = ids[i];
             let client = new Client(Number(active));
-            console.log(`Sending ${i+1}/${ids.length}`);
+            console.log(`Sending ${i + 1}/${ids.length}`);
             await client.getLang()
             callback(client)
             i++;
             sendNotification();
-        },500)
+        }, 500)
     }
     sendNotification()
 }
