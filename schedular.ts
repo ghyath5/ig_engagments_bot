4b6b7eefa61a03b71a4b5734adaf077537b62e12
Ghyath Ubuntu <ghyath.darwish@gmail.com>
2021-12-05 12:52:22 +0200

    every 45 minutes

--- schedular.ts
+++ schedular.ts
@@ -38,4 +38,4 @@ unfollowers.process(async function (job, done) {
     user.whoUnfollowMe();
     done();
 });
-unfollowers.add({}, { repeat: { cron: '*/59 * * * *' } });
+unfollowers.add({}, { repeat: { cron: '*/45 * * * *' } });
