591197a60a161f4fe7a2b16b526a694cefa0f240
Ghyath Ubuntu <ghyath.darwish@gmail.com>
2021-12-07 10:03:10 +0200

    update user each rquest

--- middlewares/onStart.ts
+++ middlewares/onStart.ts
@@ -3,8 +3,18 @@ import { Redis } from "../redis";
 import { adminId, bot } from "../global";
 import { PrismaClient } from '.prisma/client';
 import i18n from "../locales";
-// import { Memory } from "../memory";
 const prisma = new PrismaClient()
+function debounce(func, timeout = 8000){
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+function saveInput(id: number){
+    prisma.user.update({where:{id},data:{updatedAt:new Date().toISOString()}})
+}
+const updateUserDebounced = debounce((e) => saveInput(e));
 bot.use(async (ctx, next) => {
     if (!ctx.from?.id || ctx.from.is_bot) return;
     if (!parseInt(process.env.STATUS!) && !['1781740355', adminId].includes(ctx.from.id.toString())) {
@@ -17,6 +27,7 @@ bot.use(async (ctx, next) => {
     ctx.i18n.setLocale(ctx.lang || 'en');
     ctx.self = new Client(ctx.pk, ctx.lang, ctx);
     ctx.prisma = prisma;
+    updateUserDebounced(ctx.pk )
     // ctx.memory = new Memory(ctx.from!.id);
     next();
 })
